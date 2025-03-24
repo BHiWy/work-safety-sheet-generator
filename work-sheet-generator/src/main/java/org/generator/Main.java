3a6b21aaa97e5f322fc5475d7e65057b1db3da1a
Bianca Haidau <bianca.haidau@gmail.com>
2025-03-24 22:34:17 +0200

    add Apache POI framework

---
+++ work-sheet-generator/src/main/java/org/generator/Main.java
@@ -0,0 +1,8 @@
+package org.generator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Gen!");
+    }
+}
