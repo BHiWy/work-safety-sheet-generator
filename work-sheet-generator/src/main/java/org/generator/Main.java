c6ce0dfd395985dee101eb113d70c03ab549500b
Andrei - Daniel Nechifor <daniel.andrei759@gmail.com>
2025-03-28 23:41:17 +0200

    am adaugat alta linie

--- work-sheet-generator/src/main/java/org/generator/Main.java
+++ work-sheet-generator/src/main/java/org/generator/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("alexandra");
         System.out.println("Andrei");
         System.out.println("Andrei");
+        System.out.println("Andrei din nou");
 
     }
 }
