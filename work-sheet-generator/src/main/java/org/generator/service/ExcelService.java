2d421cb7f4edc7f32fb4e1d26da2df8ef69f4a10
Alexandra <alexandra-maria.cojocariu@student.tuiasi.ro>
2025-03-31 11:49:41 +0300

    functie citire

--- work-sheet-generator/src/main/java/org/generator/service/ExcelService.java
+++ work-sheet-generator/src/main/java/org/generator/service/ExcelService.java
@@ -1,33 +1,39 @@
 package org.generator.service;
 
-
-import org.apache.commons.collections4.Factory;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.generator.entities.Student;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
-public class ExcelService1 {
+public class ExcelService {
     private static final String FILE_PATH = "../resources/grupe-an-III-AIA-2024-2025.xls";
 
     public void readExcel() {
         List<Student> students = new ArrayList<>();
 
-        Factory<Object> XSSFWorkbookFactory = null;
-        try (FileInputStream fis = new FileInputStream(new File(FILE_PATH));
-             Workbook workbook = (Workbook) XSSFWorkbookFactory.create()) {
+        //Factory<Object> XSSFWorkbookFactory = null;
+        try (FileInputStream fis = new FileInputStream(new File(FILE_PATH))){
+             Workbook workbook = null;
+            if (FILE_PATH.endsWith(".xls")) {
+            workbook = new HSSFWorkbook(fis); // Fișier .xls
+        } else if (FILE_PATH.endsWith(".xlsx")) {
+
+            workbook = new XSSFWorkbook(fis); // Fișier .xlsx
+        }
 
             Sheet sheet = workbook.getSheetAt(0); // Prima foaie a Excelului
 
             // Alternativă la for clasica
             IntStream.rangeClosed(1, sheet.getLastRowNum()) // Sărim peste antet (rând 0)
                     .mapToObj(sheet::getRow) // Convertim indexul într-un rând
-                    .filter(row -> row != null) // Evităm rândurile nule
+                    .filter(Objects::nonNull) // Evităm rândurile nule
                     .forEach(row -> {
                         Cell groupCell = row.getCell(1); // A doua coloană (index 1)
                         Cell studentCell = row.getCell(8); // A noua coloană (index 8)
